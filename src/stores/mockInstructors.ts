00549df7ac879dcbed63e6f59a7f2e91ca9029f5
b12o <barny@edubio.de>
2025-05-29 12:51:41 +0200

    add mock instructors and students

---
+++ src/stores/mockInstructors.ts
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 'ljulfEag',
+    firstName: 'Brandon',
+    lastName: 'Langworth',
+    email: 'carole@gmail.com',
+    drivingSchoolId: 'Y8eTj9iP',
+  },
+  {
+    id: 'bYRTp8lM',
+    firstName: 'Kennedy',
+    lastName: 'Hoeger',
+    email: 'joseph.anderson@rbp.net',
+    drivingSchoolId: 'Y8eTj9iP',
+  },
+  {
+    id: 'qt7CYs5f',
+    firstName: 'Jimmy',
+    lastName: 'Sipes',
+    email: 'hermiston.mae@czo.com',
+    drivingSchoolId: 'Y8eTj9iP',
+  },
+  {
+    id: '5pRgKEmi',
+    firstName: 'Breana',
+    lastName: 'Corkery',
+    email: 'chris@yahoo.com',
+    drivingSchoolId: 'Y8eTj9iP',
+  },
+]
