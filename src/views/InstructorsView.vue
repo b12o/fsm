a7475679ccfb7064bdf22e99d6e8166c24e10183
b12o <barny@edubio.de>
2025-05-29 12:53:13 +0200

    visualize mock students and instructors in basic table

--- src/views/InstructorsView.vue
+++ src/views/InstructorsView.vue
@@ -1,7 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import mockInstructors from '@/stores/mockInstructors'
+</script>
 
 <template>
-  <div>Instructors View</div>
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead>Vorname</TableHead>
+        <TableHead>Nachname</TableHead>
+        <TableHead>Email</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="instructor in mockInstructors" :key="instructor.id">
+        <TableCell>{{ instructor.firstName }}</TableCell>
+        <TableCell>{{ instructor.lastName }}</TableCell>
+        <TableCell>{{ instructor.email }}</TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
 </template>
 
 <style scoped></style>
--- src/views/StudentsView.vue
+++ src/views/StudentsView.vue
@@ -1,7 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import mockStudents from '@/stores/mockStudents'
+</script>
 
 <template>
-  <div>Students View</div>
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead>Vorname</TableHead>
+        <TableHead>Nachname</TableHead>
+        <TableHead>Email</TableHead>
+        <TableHead>Telefonnummer</TableHead>
+        <TableHead>Status</TableHead>
+        <TableHead>Typ</TableHead>
+        <TableHead>BF17</TableHead>
+        <TableHead>Stunden</TableHead>
+        <TableHead>Saldo</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="student in mockStudents" :key="student.id">
+        <TableCell>{{ student.firstName }}</TableCell>
+        <TableCell>{{ student.lastName }}</TableCell>
+        <TableCell>{{ student.email }}</TableCell>
+        <TableCell>{{ student.phoneNumber }}</TableCell>
+        <TableCell>{{ student.status }}</TableCell>
+        <TableCell>{{ student.licenseType }}</TableCell>
+        <TableCell>{{ student.isBf17 }}</TableCell>
+        <TableCell>{{ student.numLessons }}</TableCell>
+        <TableCell>{{ student.balance }}</TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
 </template>
 
 <style scoped></style>
