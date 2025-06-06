143a03d8750847b33ebccdbf424065c62200ad66
b12o <barny@edubio.de>
2025-06-06 20:49:16 +0200

    outsourced info items

---
+++ src/components/InfoItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  itemKey: string
+  itemValue: string | number | boolean
+}>()
+</script>
+
+<template>
+  <div class="basis-1/5 p-2">
+    <div class="font-bold text-xs">{{ props.itemKey }}</div>
+    <div class="text-sm">{{ props.itemValue }}</div>
+  </div>
+</template>
+
+<style scoped></style>
