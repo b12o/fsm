399a7e9152be40345d72e1c1a6172140aa7e1b8a
b12o <barny@edubio.de>
2025-06-08 09:56:49 +0200

    wrap all in parent div

--- src/components/ui/data-table.vue
+++ src/components/ui/data-table.vue
@@ -62,69 +62,71 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex items-center py-4">
-    <span class="absolute start-2 flex items-center justify-center px-2">
-      <Search class="size-5 text-muted-foreground" />
-    </span>
-    <Input
-      id="data-table-search-input"
-      class="max-w-sm pl-8"
-      placeholder="Suche"
-      @update:model-value="table.setGlobalFilter($event)"
-    />
-  </div>
-  <div class="border rounded-md">
-    <Table>
-      <TableHeader>
-        <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
-          <TableHead v-for="header in headerGroup.headers" :key="header.id">
-            <FlexRender
-              v-if="!header.isPlaceholder"
-              :render="header.column.columnDef.header"
-              :props="header.getContext()"
-            />
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <template v-if="table.getRowModel().rows?.length">
-          <TableRow
-            v-for="row in table.getRowModel().rows"
-            :key="row.id"
-            :data-state="row.getIsSelected() ? 'selected' : 'undefined'"
-            @click="rowSelect(row)"
-          >
-            <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-              <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
-            </TableCell>
+  <div>
+    <div class="flex items-center py-4">
+      <span class="absolute start-2 flex items-center justify-center px-2">
+        <Search class="size-5 text-muted-foreground" />
+      </span>
+      <Input
+        id="data-table-search-input"
+        class="max-w-sm pl-8"
+        placeholder="Suche"
+        @update:model-value="table.setGlobalFilter($event)"
+      />
+    </div>
+    <div class="border rounded-md">
+      <Table>
+        <TableHeader>
+          <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+            <TableHead v-for="header in headerGroup.headers" :key="header.id">
+              <FlexRender
+                v-if="!header.isPlaceholder"
+                :render="header.column.columnDef.header"
+                :props="header.getContext()"
+              />
+            </TableHead>
           </TableRow>
-        </template>
-        <template v-else>
-          <TableRow>
-            <TableCell :colspan="columns.length" class="h-24 text-center">
-              Wird geladen...
-            </TableCell>
-          </TableRow>
-        </template>
-      </TableBody>
-    </Table>
-  </div>
-  <div class="flex items-center justify-center py-4 space-x-2">
-    <Button
-      variant="outline"
-      size="sm"
-      :disabled="!table.getCanPreviousPage()"
-      @click="table.previousPage()"
-    >
-      <ChevronLeft />
-    </Button>
-    <Button
-      variant="outline"
-      size="sm"
-      :disabled="!table.getCanNextPage()"
-      @click="table.nextPage()"
-    >
-      <ChevronRight />
-    </Button>
+        </TableHeader>
+        <TableBody>
+          <template v-if="table.getRowModel().rows?.length">
+            <TableRow
+              v-for="row in table.getRowModel().rows"
+              :key="row.id"
+              :data-state="row.getIsSelected() ? 'selected' : 'undefined'"
+              @click="rowSelect(row)"
+            >
+              <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+                <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+              </TableCell>
+            </TableRow>
+          </template>
+          <template v-else>
+            <TableRow>
+              <TableCell :colspan="columns.length" class="h-24 text-center">
+                Wird geladen...
+              </TableCell>
+            </TableRow>
+          </template>
+        </TableBody>
+      </Table>
+    </div>
+    <div class="flex items-center justify-center py-4 space-x-2">
+      <Button
+        variant="outline"
+        size="sm"
+        :disabled="!table.getCanPreviousPage()"
+        @click="table.previousPage()"
+      >
+        <ChevronLeft />
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        :disabled="!table.getCanNextPage()"
+        @click="table.nextPage()"
+      >
+        <ChevronRight />
+      </Button>
+    </div>
   </div>
 </template>
