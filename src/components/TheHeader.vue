df319d83e3b0833cfe386e9ce16e26b0e67e1b78
b12o <barny@edubio.de>
2025-07-10 19:32:19 +0200

    remove breadcrumb and separator from header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,34 +1,13 @@
 <script setup lang="ts">
 import { SidebarTrigger } from '@/components/ui/sidebar'
-import { Separator } from '@/components/ui/separator'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb'
 
-import { useSidebarStore } from '@/stores/sidebarStore'
 import TheModeSwitch from './TheModeSwitch.vue'
-
-const store = useSidebarStore()
 </script>
 
 <template>
   <header class="flex h-16 shrink-0 items-center">
     <div class="flex items-center gap-2 px-4 w-full">
       <SidebarTrigger class="-ml-1" />
-      <Separator orientation="vertical" class="mr-2 h-4" />
-      <Breadcrumb class="mt-0.5">
-        <BreadcrumbList>
-          <BreadcrumbItem class="hidden md:block"> Übersicht </BreadcrumbItem>
-          <BreadcrumbSeparator class="hidden md:block" />
-          <BreadcrumbItem>
-            <BreadcrumbPage>{{ store.sidebar.currentView }}</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
       <TheModeSwitch class="ml-auto mr-8" />
     </div>
   </header>
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  // plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     // alias: {
     // '@': fileURLToPath(new URL('./src', import.meta.url))
